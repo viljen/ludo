a141b98848b3303d3a5498db050c98a80bee98da
Sindre Bjørndal-Riis <103846992+sindresbr@users.noreply.github.com>
2022-10-11 00:29:25 +0200

    Add files via upload

--- ludo.py
+++ ludo.py
@@ -8,6 +8,102 @@ start = np.array([[1,1,1,1],[2,2,2,2],[3,3,3,3],[4,4,4,4]])
 atStart = [0,0,0,0]
 die = 0
 
+def printboard():
+    text = ""
+    text += "      "
+    for i in range(3):
+        text += str(int(board[1][0][i]))
+    text += "      \n"
+    text += "      "
+    for i in range(3):
+        text += str(int(board[1][1][i]))
+    text += "      \n"
+    text += "  "
+    for i in range(2):
+        text += str(int(start[0][i]))
+    text += "  "
+    for i in range(3):
+        text += str(int(board[1][2][i]))
+    text += "  "
+    for i in range(2):
+        text += str(int(start[1][i]))
+    text += "  \n"
+    text += "  "
+    for i in range(2):
+        text += str(int(start[0][i+2]))
+    text += "  "
+    for i in range(3):
+        text += str(int(board[1][3][i]))
+    text += "  "
+    for i in range(2):
+        text += str(int(start[1][i+2]))
+    text += "  \n"
+    text += "      "
+    for i in range(3):
+        text += str(int(board[1][4][i]))
+    text += "      \n"
+    text += "      "
+    for i in range(3):
+        text += str(int(board[1][5][i]))
+    text += "      \n"
+    for i in range(6):
+        text += str(int(board[0][i][2]))
+    text += "   "
+    for i in range(6):
+        text += str(int(board[2][5-i][0]))
+    text += "\n"
+    for i in range(6):
+        text += str(int(board[0][i][1]))
+    text += "   "
+    for i in range(6):
+        text += str(int(board[2][5-i][1]))
+    text += "\n"
+    for i in range(6):
+        text += str(int(board[0][i][0]))
+    text += "   "
+    for i in range(6):
+        text += str(int(board[2][5-i][2]))
+    text += "\n"
+    text += "      "
+    for i in range(3):
+        text += str(int(board[3][5][2-i]))
+    text += "      \n"
+    text += "      "
+    for i in range(3):
+        text += str(int(board[3][4][2-i]))
+    text += "      \n"
+    text += "  "
+    for i in range(2):
+        text += str(int(start[3][i]))
+    text += "  "
+    for i in range(3):
+        text += str(int(board[3][3][2-i]))
+    text += "  "
+    for i in range(2):
+        text += str(int(start[2][i]))
+    text += "  \n"
+    text += "  "
+    for i in range(2):
+        text += str(int(start[3][i+2]))
+    text += "  "
+    for i in range(3):
+        text += str(int(board[3][2][2-i]))
+    text += "  "
+    for i in range(2):
+        text += str(int(start[2][i+2]))
+    text += "  \n"
+    text += "      "
+    for i in range(3):
+        text += str(int(board[3][1][2-i]))
+    text += "      \n"
+    text += "      "
+    for i in range(3):
+        text += str(int(board[3][0][2-i]))
+    text += "      \n\n"
+    print(text)
+    print("Goal:")
+    print(goal)
+
 def rolldie():
     return random.randint(1,6)
 
@@ -38,7 +134,6 @@ def newmove(k, j, g, die, i):
                 index -= 1
                 new_j = 0
                 new_g = 1
-                print(index)
                 newmove(k, new_j, new_g, index, i)
             else:
                 new_j = 0
@@ -47,8 +142,6 @@ def newmove(k, j, g, die, i):
             new_j = move
     elif g == 1:
         if k == i-1:
-            print("Vi nærmer oss mål")
-            print(board)
             if j + die <= 5:
                 new_j = j + die
             else:
@@ -56,7 +149,7 @@ def newmove(k, j, g, die, i):
                 print(goal)
                 while goal[i-1][t] == i:
                     t += 1
-                goal[i-1][t] = i
+                goal[i-1][t] = int(i)
                 new_g = 8
                 new_j = 8
                 new_k = 8
@@ -92,9 +185,7 @@ def kick(k, j, g, i, die, old_k, old_j, old_g):
             board[k][j][g] = 0
             board[new_k][new_j][new_g] = i
     else:
-        print(kicked)
         index = 0
-        print(start[kicked-1])
         while start[kicked-1][index] != 0:
             index += 1
         start[kicked-1][index] = kicked
@@ -102,9 +193,7 @@ def kick(k, j, g, i, die, old_k, old_j, old_g):
         board[k][j][g] = i
 
 def moveboard(i, die):
-    #print("Moving board")
     if i in board:
-        print(f"{i} moving")
         k, j, g = findplace(i)
         new_k, new_j, new_g = newmove(k,j,g,die,i)
         if new_k != 8:
@@ -116,12 +205,10 @@ def moveboard(i, die):
         else:
             board[k][j][g] = 0
     if atStart[i-1] != 0:
-        print(atStart)
         if board[i-1][1][2] == 0:
             atStart[i-1] -= 1
             if atStart[i-1] != 0:
                 board[i-1][1][2] = i
-    #print(board)
 
 def makemove(i):
     print(f"Rolling for {i}")
@@ -133,33 +220,32 @@ def makemove(i):
             while start[i-1][j] != i:
                 j += 1
             start[i-1][j] = 0
-            print(f"{i} Got out of start")
             board[i-1][1][2] = i
-            #print(board)
             atStart[i-1] += 1
+            printboard()
         else:
             moveboard(i, die)
+            printboard()
+        input()
         makemove(i)
     else:
         moveboard(i, die)
+        printboard()
 
 won = False
 winner = 0
 while won == False:
 #for i in range(85):
-    for i in range(1,5):
+    i = 1
+    while i < 5:
         makemove(i)
         if 0 not in goal[i-1]:
             won = True
             winner = i
-            #exit()
-        print(board)
-        #time.sleep(10)
+            i = 5
+        #printboard()
+        i += 1
+        input()
 
 print("End")
-print(board)
-print(start)
-print(goal)
-print(atStart)
-print(won)
-print(winner)
+print(f"{winner} won")
