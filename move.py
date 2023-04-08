efae384a8e1ee682b79f326ed620fc99ed015485
viljen <tor@viljen.net>
2023-04-08 21:10:55 +0200

    Implement some move-property methods.

--- move.py
+++ move.py
@@ -7,19 +7,54 @@ class Move:
         
 
     def execute():
+        # Perform the move on the board
         self.start_field.remove_pawn(self.active_pawn)
         if self.capture:
             for pawn in end_field.pawns:
                 self.end_field.remove_pawn(pawn)
                 pawn.isOn = None
         self.end_field.add_pawn(self.active_pawn)
+
+    # The following methods describe the properties of the move
         
     def capture():
         # Is it a capture move?
-        for pawn in end_field.pawns:
-            if (pawn.belongsTo is not active_pawn.belongsTo):
+        for pawn in self.end_field.pawns:
+            if (pawn.belongsTo is not self.active_pawn.belongsTo):
                 return True
         return False
+
+    def goalMove():
+        # Is it a move within the goal area?
+        return self.start_field.goal != None
+
+    def intoGoal():
+        # Does the move make the pawn enter the goal area?
+        return (not self.goalMove()) and (self.end_field.goal != None)
+
+    def createStack():
+        # Does the move create a safe stack?
+        for pawn in self.end_field.pawns:
+            if (pawn.belongsTo is self.active_pawn.belongsTo):
+                return True
+        return False
+    
+    def breakStack():
+        # Does the move break a safe stack?
+        pawnCount = 0
+        for pawn in self.start_field.pawns:
+            if (pawn.belongsTo is self.active_pawn.belongsTo):
+                pawnCount += 1
+        return pawnCount == 2
+
+    def endSafe():
+        # Does the move end in safety?
+        return self.createStack() or self.goalMove() or self.intoGoal()
+
+    def startSafe():
+        # Does the move start in safety?
+        inStack = False
+
         
     def __str__(self):
         capture_str = ", with capture" if self.Capture else ""
